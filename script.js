ed176fe39f9fcec3becf87b19af4e5ff49dd5dce
Prince Balyan <princebalyan9341@gmail.com>
2023-11-17 00:29:52 +0530

    animation v2

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get all elements with the 'animate' class
-    var animatedElements = document.querySelectorAll('.animate');
-
     // Initialize ScrollMagic controller
     var controller = new ScrollMagic.Controller();
 
-    // Iterate through each animated element and create a ScrollMagic scene
-    animatedElements.forEach(function (element) {
-        new ScrollMagic.Scene({
-            triggerElement: element,
-            triggerHook: 0.8, // Adjust the triggerHook as needed
-            reverse: false // Change to true if you want the animation to reverse on scrolling up
-        })
-        .setClassToggle(element, 'active') // Add the 'active' class on scroll
-        .addTo(controller);
-    });
+    // Create a TweenMax animation
+    var tween = TweenMax.fromTo("#name", 1, { opacity: 0, x: -100 }, { opacity: 1, x: 0 });
+
+    // Create a ScrollMagic scene
+    var scene = new ScrollMagic.Scene({
+        triggerElement: "#name",  // The element that triggers the animation
+        triggerHook: 0.5,  // Trigger the animation at the center of the viewport
+        reverse: false  // Allow the animation to reverse when scrolling up
+    })
+    .setTween(tween)  // Set the TweenMax animation
+    .addTo(controller);  // Add the scene to the ScrollMagic controller
 });
